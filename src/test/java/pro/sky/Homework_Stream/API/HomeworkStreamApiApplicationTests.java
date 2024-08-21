c603042ca90afeb8bb6a64a17a183c8d05befcbb
Sergey Makarov <nucler-makar@rambler.ru>
2024-08-21 13:40:34 +0300

    Основа основ.

---
+++ src/test/java/pro/sky/Homework_Stream/API/HomeworkStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_Stream.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
