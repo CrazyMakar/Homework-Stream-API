db89ff2714606a119d7e9d5d1bfe08ffba081150
Sergey Makarov <nucler-makar@rambler.ru>
2024-08-21 13:38:10 +0300

    Основа основ.

---
+++ src/main/java/pro/sky/Homework_Stream/API/HomeworkStreamApiApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_Stream.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkStreamApiApplication.class, args);
+	}
+
+}
